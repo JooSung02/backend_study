bfd7bd79b7d2ec8ece5766948cf598a4094405a9
Juseong Park <wntjdqkr156@gmail.com>
2025-07-10 15:02:54 +0900

    html_template

--- 01_routing/app.py
+++ 01_routing/app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
@@ -8,7 +8,7 @@ def home():
 
 @app.route('/hello/<name>')
 def greet(name):
-    return f'Hello, {name}!'
+    return render_template('hello.html', name=name)
 
 if __name__ == '__main__':
     app.run(debug=True) 
